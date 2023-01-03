3b690bffdd5c9b2230ba6ad50f6416666c15213b
Albin1118 <31730135+Albin1118@users.noreply.github.com>
2023-01-03 15:51:56 +0100

    move vocab functionality to dataset class

--- flickerdata.py
+++ flickerdata.py
@@ -3,6 +3,10 @@ import torch
 import torchvision
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
+from collections import Counter
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 import pandas as pd
 import time
 
@@ -11,10 +15,13 @@ vgg_transform = torchvision.models.VGG19_Weights.IMAGENET1K_V1.transforms()
 
 class FlickerData(Dataset):
 
-    def __init__(self, subset):
+    def __init__(self, subset, vocab=None, max_vocab_len=None):
 
         if subset.lower() not in ["train", "dev", "test"]:
             raise ValueError("Not valid option.")
+
+        self.vocab = vocab
+        self.max_vocab_len = max_vocab_len
 
         image_path = f"Data\Flicker8k_text\Flickr_8k.{subset.lower()}Images.txt"
         lemma_caption_path = f"Data\Flicker8k_text\Flickr8k.lemma.token.txt"
@@ -48,7 +55,6 @@ class FlickerData(Dataset):
 
         # Transform images from PIL to torch tensor & search for labels
         for img_idx, image_file in enumerate(image_handles):
-
             # Images
             pil_image = Image.open(f"Data\Flicker8k_images\{image_file}")
             transformed_image = vgg_transform(pil_image)
@@ -60,22 +66,60 @@ class FlickerData(Dataset):
 
             self.labels.append((lemma_caption, caption))
 
+        if vocab is None:
+            vocab = self.make_vocab(self.labels)
+
+
+
     def __getitem__(self, index):
         """ Returns tuple (image, lemma_caption, caption) """
+        # (dataset_size, img, caption)[i] -> (tensor(3,224,224), tensor(max_sen_len), tensor(max_sen_len))
         return (self.images[index, :, :, :], self.labels[index][0], self.labels[index][1])  # (torch.tensor, str, str)
 
     def __len__(self):
         return self.n_images
 
+    def make_vocab(self, data):
+        """ Creates a vocabulary using training data.
+                                    Special symbols, <BOS>,<EOS>,<PAD>,<UNK> """
+
+        vocab = {}
+        # s_vocab = len(self.max_vocab_len)
+
+        UNKNOWN = "<UNKNOWN>"
+        BOS = "<BOS>"
+        EOS = "<EOS>"
+        PAD = "<PAD>"
+
+        lemma_captions = []
+        for i in range(self.__len__()):
+            lemma_captions.append(self.data[i][0])  # append lemma caption
+
+        word_count = Counter(t for x in lemma_captions for t in [t.text.lower() for t in nlp.tokenizer(x)])
+
+        vocab[UNKNOWN] = 0
+        vocab[BOS] = 1
+        vocab[EOS] = 2
+        vocab[PAD] = 3
+
+        if self.max_vocab_len is not None:
+            word_list = word_count.most_common(self.max_vocab_len - 4)
+        else:
+            word_list = word_count
+
+        for i, (w, count) in enumerate(word_list):
+            self.vocab[w] = i + 4
+
+        return vocab
+
 
 if __name__ == "__main__":
-
     t0 = time.time()
-    loader_data = DataLoader(dataset=FlickerData(subset="train"), batch_size=3) # dataloader
+    loader_data = DataLoader(dataset=FlickerData(subset="train"), batch_size=3)  # dataloader
     t_end = time.time() - t0
     print(f"Data loading time {t_end:.3f} s")
     iter_loader = iter(loader_data)
-    images, lemmas, captions = next(iter_loader) # images: tensor, lemmas: tuple, captions: tuple
-    print(images.shape) # batch_size, n_channels, width, height
+    images, lemmas, captions = next(iter_loader)  # images: tensor, lemmas: tuple, captions: tuple
+    print(images.shape)  # batch_size, n_channels, width, height
     print(lemmas)
     print(captions)
--- model.py
+++ model.py
@@ -1,12 +1,11 @@
 import torch
 import torch.nn as nn
 import torchvision
-import Params
-import spacy
+from torch.utils.data import DataLoader
 
-nlp = spacy.load("en_core_web_sm")
+from Params import Params
 from flickerdata import FlickerData
-from collections import Counter
+from Vocabulary import Vocabulary
 
 
 class Encoder(torch.nn.Module):
@@ -15,22 +14,22 @@ class Encoder(torch.nn.Module):
     def __init__(self, p):
         super().__init__()
 
-        self.vgg = torchvision.models.vgg19() #  load in encoder
-        for _, param in self.vgg.named_parameters(): # freeze vgg weights
+        self.vgg = torchvision.models.vgg19()  # load in encoder
+        for _, param in self.vgg.named_parameters():  # freeze vgg weights
             param.requires_grad = False
 
-        self.latent_embedding = nn.Linear(4096, p.emb_dim) # maps latent_vector to embed_vector
-        
+        self.latent_embedding = nn.Linear(4096, p.emb_dim)  # maps latent_vector to embed_vector
+
     def forward(self, X):
         """ Takes in image X and outputs embed_vector """
 
         batch_size = X.shape[0]
 
-        X = self.vgg.features(X) # convolutional part
-        X = self.vgg.avgpool(X) # sets to fixed size (512,7,7) 
-        X = X.view(batch_size, 512*7*7) # (batch,25088) flatten
-        X = self.vgg.classifier[0:3](X) # (batch,4096)
-        X = self.latent_embedding(X) # (batch,emb_dim)
+        X = self.vgg.features(X)  # convolutional part
+        X = self.vgg.avgpool(X)  # sets to fixed size (512,7,7)
+        X = X.view(batch_size, 512 * 7 * 7)  # (batch,25088) flatten
+        X = self.vgg.classifier[0:3](X)  # (batch,4096)
+        X = self.latent_embedding(X)  # (batch,emb_dim)
         return X
 
 
@@ -42,11 +41,11 @@ class Decoder(torch.nn.Module):
 
         self.p = p
         self.lstm = nn.LSTM(
-            input_size= p.emb_dim,
-            hidden_size= p.hidden_size,
-            num_layers= p.n_layers,
-            batch_first= True)
-        self.word_emb = nn.Embedding(p.vocab_size, p.emb_dim) 
+            input_size=p.emb_dim,
+            hidden_size=p.hidden_size,
+            num_layers=p.n_layers,
+            batch_first=True)
+        self.word_emb = nn.Embedding(p.vocab_size, p.emb_dim)
         self.output_emb = nn.Linear(p.hidden_size, p.vocab_size)
 
     def forward(self, embed_vector, caption):
@@ -59,85 +58,67 @@ class Decoder(torch.nn.Module):
         """
 
         batch_size = embed_vector.shape[0]
-        embed_captions = self.word_emb(caption) # (batch_size, max_sen_len, emb_dim)
-        inputs = torch.cat( 
+        embed_captions = self.word_emb(caption)  # (batch_size, max_sen_len, emb_dim)
+        inputs = torch.cat(
             (embed_vector.view(batch_size, 1, self.p.emb_dim), embed_captions),
-            dim= 1) # (batch_size, max_sen_len+1, emb_dim)
-        output, _ = self.lstm(inputs) # (batch_size, max_sen_len+1, hidden_size)
-        logits = self.output_emb(output) # (batch_size, max_sen_len+1, vocab_size)
+            dim=1)  # (batch_size, max_sen_len+1, emb_dim)
+        output, _ = self.lstm(inputs)  # (batch_size, max_sen_len+1, hidden_size)
+        logits = self.output_emb(output)  # (batch_size, max_sen_len+1, vocab_size)
         return logits
 
     def predict(self, latent_embedding):
         """ Used to predict caption for images during testing """
         sentence = []
-        lstm_out, _ = self.lstm(latent_embedding) # (batch, emb_dim) -> (batch_size, seq_len, hidden_size)
+        lstm_out, _ = self.lstm(latent_embedding)  # (batch, emb_dim) -> (batch_size, seq_len, hidden_size)
         prob = self.output_emb(lstm_out)
-        norm_prob = nn.functional.softmax(prob) 
+        norm_prob = nn.functional.softmax(prob)
 
         # sample word (greedy)
 
         next_token = norm_prob.argmax()
         sentence.append(next_token)
-        
+
         sentence_length = 1
         while (sentence_length <= self.p.max_sen_len):
-            emb_token = self.word_emb(next_token) # integer-representation -> word_embedding
-            lstm_out, _ = self.lstm(emb_token) 
+            emb_token = self.word_emb(next_token)  # integer-representation -> word_embedding
+            lstm_out, _ = self.lstm(emb_token)
             prob = self.output_emb(lstm_out)
-            norm_prob = nn.functional.softmax(prob) 
+            norm_prob = nn.functional.softmax(prob)
             next_token = norm_prob.argmax()
             sentence.append(next_token)
             sentence_length += 1
             if (next_token == "<EOS>"):
                 break
 
-        return sentence 
+        return sentence
 
 
 class ImageCaptionGenerator(torch.nn.Module):
 
-    def __init__(self, mode, p):
-        if mode not in ["train", "dev" "test"]:
-            raise ValueError(f"Incorrect mode {mode}")
-        self.vocab = self.make_vocab(FlickerData(mode))
+    def __init__(self, p: Params):
+        t_data = FlickerData("train")
+        #vocab = t_data.get_vocab()
+        #val_data = FlickerData("dev", vocab=vocab)
+        train_dataloader = DataLoader(dataset=t_data, batch_size=p.batch_size)
+        val_dataloader = DataLoader(dataset=FlickerData(subset="dev"), batch_size=p.batch_size)
+        test_dataloader = DataLoader(dataset=FlickerData(subset="test"), batch_size=p.batch_size)
+
+        self.vocab = self.make_vocab(t_data)
 
         self.encoder = Encoder(p)
         self.decoder = Decoder(p)
-        self.loss = None # CE
-        self.optim = None # ADAMS ? 
+        self.loss = None  # CE
+        self.optim = None  # ADAMS ?
         pass
 
     def make_vocab(self, data):
-        """ Creates a vocabulary using training data.
-            Special symbols, <BOS>,<EOS>,<PAD>,<UNK> """
-
-        UNKNOWN = "<UNKNOWN>"
-        BOS = "<BOS>"
-        EOS = "<EOS>"
-
-        lemma_captions = []
-        for i in range(self.n_images):
-            lemma_captions.append(self.data[i][1])  # append lemma caption
-
-        freqs = Counter(t for x in lemma_captions for t in [t.text.lower() for t in nlp.tokenizer(x)])
-
-        vocab = {}
-        vocab[UNKNOWN] = 0
-        vocab[BOS] = 1
-        vocab[EOS] = 2
-
-        freq_list = freqs
-
-        for i, (w, count) in enumerate(freq_list):
-            self.vocab_x[w] = i + 3
-
-        return vocab
+        return Vocabulary(data).get_vocab()
 
     def forward(self, image):
         """ Takes in images and returns probability distribution for each token """
         latent_embedding = self.encoder(image)
         return self.decoder(latent_embedding)
-        
+
     def predict(self, image):
         """ Takes in image and returns caption """
         latent_image = self.encoder(image)
@@ -146,17 +127,17 @@ class ImageCaptionGenerator(torch.nn.Module):
     def train(self, n_epochs):
         pass
 
+
 if __name__ == "__main__":
-        
     batch_size = 8
-    p = Params.Params(vocab_size= 100)
+    p = Params.Params(vocab_size=100)
 
     # Encoder testing
     encoder = Encoder(p)
-    A = torch.rand(batch_size,3,250,300) # (batch, channel, width, height) 
+    A = torch.rand(batch_size, 3, 250, 300)  # (batch, channel, width, height)
     A_out = encoder(A)
     print(A_out.shape)
 
     # Decoder testing
-    #decoder = Decoder(p)
-    #B_out = decoder(A_out)
+    # decoder = Decoder(p)
+    # B_out = decoder(A_out)
